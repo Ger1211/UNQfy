3966147084634a47f0dbb0a5f31078b3c06ae227
Facundo Sardi <facusardi96@gmail.com>
2021-09-20 18:04:17 -0300

    push de prueba

--- unqfy.js
+++ unqfy.js
@@ -33,6 +33,8 @@ class UNQfy {
     this.nextUserId = 1;
   }
 
+  //ALGO
+
   // artistData: objeto JS con los datos necesarios para crear un artista
   //   artistData.name (string)
   //   artistData.country (string)
